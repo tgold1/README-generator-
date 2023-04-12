b83cdade10b0d6e4e95673674fdaf1ffd00b69c1
Tim Gold <timothygold@comcast.net>
2023-04-12 19:17:02 -0400

    linked the responses from the prompts to the input fields in the generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,50 +12,39 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  # <Your-Project-Title>
+  return ` 
+  # ${data.title}
 
   ## Description
-  
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-  
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
+    ${data.description}
+
   
   ## Table of Contents (Optional)
+    ${data.tableofcontents}
   
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
+-  [Installation](#installation)
+-  [Usage](#usage)
+-  [Credits](#credits)
+-  [License](#license)
   
   ## Installation
   
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+   ${data.installation}
   
   ## Usage
   
-  Provide instructions and examples for use. Include screenshots as needed.
+   ${data.usage}
   
  
   
   ## Credits
   
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
+   ${data.contributing}
   
   ## License
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+   ${data.license}
   
-  ---
   
   🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
   
@@ -74,7 +63,7 @@ function generateMarkdown(data) {
   If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
   
   ## Tests
-  
+  ${data.tests}
 `;
 }
 
